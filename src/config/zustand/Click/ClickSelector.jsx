5056ea64a08ff6820f59576bee5dbd4ddb9c51c8
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-05-25 20:17:08 +0700

    fix: deleted slice and selector (wrong name)

--- src/config/zustand/Click/ClickSelector.jsx
+++
@@ -1,3 +0,0 @@
-import { useBoundStore } from "../store";
-
-export const isClick = useBoundStore(state => state.isClick)
--- src/config/zustand/Click/ClickSlice.jsx
+++
@@ -1,4 +0,0 @@
-export const createClickSlice = set => ({
-    isClick: false,
-    onClick: () => set(state => ({ isClick: !state.isClick }))
-})
