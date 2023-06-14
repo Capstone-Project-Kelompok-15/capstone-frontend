063ce6d76e422150e18ff4a1bc5dfd3fc6c34866
David Ezra Prasetyo <2020104954@student.kalbis.ac.id>
2023-06-14 09:50:58 +0700

    merge slicing-david with development

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -3,7 +3,6 @@ export { default as CounterThreadReport } from "./CounterThreadReport/CounterThr
 export { default as Hero } from "./LandingPage/Hero.molecule";
 export { default as Footer } from "./Footer/Footer.molecule";
 export { default as SearchBar } from "./SearchBar/SearchBar.molecule";
-export { default as SearchInput } from "./SearchInput/SearchInput.molecule";
 export { default as CardStats } from "./CardStats/CardStats.molecule";
 export { default as UserProfile } from "./UserProfile/UserProfile.molecule";
 export { default as LaporanThread } from "./LaporanThread/LaporanThread.molecules";
