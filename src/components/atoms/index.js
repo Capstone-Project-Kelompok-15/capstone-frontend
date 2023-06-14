6072788b450c5a84c1baad034bf4e76fac74d2c7
Alieza Nuriman <muhammadalieza4@gmail.com>
2023-06-14 18:53:34 +0700

    fix: delete unneccesary files

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -5,8 +5,6 @@ export { default as DeleteSearchHistoryButton } from "./DeleteSearchHistoryButto
 export { default as MuteButton } from "./MuteButton/MuteButton.atom";
 export { default as ProfilePicture } from "./ProfilePicture/ProfilePicture.atom";
 export { default as SearchButton } from "./SearchButton/SearchButton.atom";
-export { default as UnblockButton } from "./UnblockButton/UnblockButton.atom";
-export { default as UnmuteButton } from "./UnmuteButton/UnmuteButton.atom";
 export { default as ViewButton } from "./ViewButton/ViewButton.atom";
 export { default as DeleteButton } from "./DeleteButton/DeleteButton.atom";
 export { default as ReturnButton } from "./ReturnButton/ReturnButton.atom";
