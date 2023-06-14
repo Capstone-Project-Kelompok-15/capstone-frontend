8528c5d6e904ae770ac7d74d985e02f74e3cb383
Hafiyyan Revi <hafiyyanrevi@student.telkomuniversity.ac.id>
2023-06-14 20:24:55 +0700

    fixing: slicing revi with development

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 export { default as AcceptButton } from "./AcceptButton/AcceptButton.atom"
 export { default as DenyButton } from "./DenyButton/DenyButton.atom"
 export { default as ButtonMasuk } from "./MasukButton/ButtonMasuk.atom"
-export { default as BlokirButton } from "./BlockButton/BlokirButton.atom"
 export { default as DeleteSearchHistoryButton } from "./DeleteSearchHistoryButton/DeleteSearchHistoryButton.atom"
 export { default as MuteButton } from "./MuteButton/MuteButton.atom"
 export { default as ProfilePicture } from "./ProfilePicture/ProfilePicture.atom"
 export { default as SearchButton } from "./SearchButton/SearchButton.atom"
-export { default as UnblockButton } from "./UnblockButton/UnblockButton.atom"
-export { default as UnmuteButton } from "./UnmuteButton/UnmuteButton.atom"
 export { default as ViewButton } from "./ViewButton/ViewButton.atom"
 export { default as DeleteButton } from "./DeleteButton/DeleteButton.atom"
 export { default as ReturnButton } from "./ReturnButton/ReturnButton.atom"
@@ -16,22 +12,6 @@ export { default as KeluarButton } from "./KeluarButton/KeluarButton.atom"
 export { default as Logo } from "./Logo/Logo.atom"
 export { default as ProfileButton } from "./ProfileButton/ProfileButtton.atom"
 export { default as BackButton } from "./BackButton/BackButton.atom"
-=======
-export { default as AcceptButton } from "./AcceptButton/AcceptButton.atoms";
-export { default as DenyButton } from "./DenyButton/DenyButton.atoms";
-export { default as ButtonMasuk } from "./MasukButton/ButtonMasuk.atom";
-export { default as DeleteSearchHistoryButton } from "./DeleteSearchHistoryButton/DeleteSearchHistoryButton.atom";
-export { default as MuteButton } from "./MuteButton/MuteButton.atom";
-export { default as ProfilePicture } from "./ProfilePicture/ProfilePicture.atom";
-export { default as SearchButton } from "./SearchButton/SearchButton.atom";
-export { default as ViewButton } from "./ViewButton/ViewButton.atom";
-export { default as DeleteButton } from "./DeleteButton/DeleteButton.atom";
-export { default as ReturnButton } from "./ReturnButton/ReturnButton.atom";
-export { default as KeluarButton } from "./KeluarButton/KeluarButton.atom";
-export { default as Logo } from "./Logo/Logo.atom";
-export { default as ProfileButton } from "./ProfileButton/ProfileButtton.atom";
-export { default as BackButton } from "./BackButton/BackButton.atom";
->>>>>>> origin/development
 export { default as ThreadPicture } from "./ThreadPicture/ThreadPicture.atom"
-export { default as TooltipButton } from "./TooltipButton/TooltipButton.atom";
-export { default as Button } from "./Button/Button.atom";
+export { default as TooltipButton } from "./TooltipButton/TooltipButton.atom"
+export { default as Button } from "./Button/Button.atom"
