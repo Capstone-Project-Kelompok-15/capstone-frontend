100be80d9713803505c65afb36bf73cd7521ca02
dimasandriano <duangdeong@gmail.com>
2023-06-14 18:25:11 +0700

    feat: add id in sidebar and homeview

--- src/components/organisms/HomeStats/HomeStats.organism.jsx
+++ src/components/organisms/HomeStats/HomeStats.organism.jsx
@@ -1,37 +1,41 @@
-import React from "react";
-import { CardStats } from "../../molecules";
+import React from "react"
+import { CardStats } from "../../molecules"
 
 function HomeStats() {
-	return (
-		<div>
-			<div className="w-full flex justify-between items-center">
-				<CardStats
-					statsNumber={99999}
-					statsTitle="Jumlah Pengguna"
-					Link="/manageUser/total"
-					hover
-				/>
-				<CardStats
-					statsNumber={99999}
-					statsTitle="Pengguna Online"
-					Link="/manageUser/online"
-					hover
-				/>
-				<CardStats
-					statsNumber={99999}
-					statsTitle="Jumlah Thread"
-					Link="/asd"
-					hover
-				/>
-				<CardStats
-					statsNumber={99999}
-					statsTitle="Thread Yang Dilaporkan"
-					Link="/asd"
-					hover
-				/>
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <div className="w-full flex justify-between items-center">
+                <CardStats
+                    statsNumber={99999}
+                    statsTitle="Jumlah Pengguna"
+                    Link="/manageUser/total"
+                    hover
+                    id="home-total"
+                />
+                <CardStats
+                    statsNumber={99999}
+                    statsTitle="Pengguna Online"
+                    Link="/manageUser/online"
+                    hover
+                    id="home-online"
+                />
+                <CardStats
+                    statsNumber={99999}
+                    statsTitle="Jumlah Thread"
+                    Link="/thread"
+                    hover
+                    id="home-thread"
+                />
+                <CardStats
+                    statsNumber={99999}
+                    statsTitle="Thread Yang Dilaporkan"
+                    Link="/thread/report"
+                    hover
+                    id="home-report"
+                />
+            </div>
+        </div>
+    )
 }
 
-export default HomeStats;
+export default HomeStats
