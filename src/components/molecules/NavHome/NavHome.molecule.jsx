290b62028dc735931fe5021a7b55c599497d1b95
eldavina <125462461+eldavina@users.noreply.github.com>
2023-06-23 17:08:44 +0700

    Update NavHome.molecule.jsx

--- src/components/molecules/NavHome/NavHome.molecule.jsx
+++ src/components/molecules/NavHome/NavHome.molecule.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import ProfileButtton from "../../atoms/ProfileButton/ProfileButtton.atom"
 import line from "../../../assets/line.png"
-import { KeluarButtonView } from "../../../views"
+import { KeluarButton } from "../../atoms"
 
-function NavHome() {
+function NavHome({ onClick }) {
     return (
         <div className="flex items-center gap-[25px] pt-[2px] " id="navbarHome">
             <ProfileButtton />
             <img src={line} alt="" className="h-6" />
 
-            <KeluarButtonView />
+            <KeluarButton onClick={onClick} />
         </div>
     )
 }
