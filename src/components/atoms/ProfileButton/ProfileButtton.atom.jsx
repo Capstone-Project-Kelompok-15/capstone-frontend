f282f51f2ca649bbd35315ea611ebaf7b7d1fd99
eldavina <eldaaavina@gmail.com>
2023-06-23 17:40:20 +0700

    fix navbar

--- src/components/atoms/ProfileButton/ProfileButtton.atom.jsx
+++ src/components/atoms/ProfileButton/ProfileButtton.atom.jsx
@@ -14,7 +14,8 @@ function ProfileButtton() {
                 <img src={pp1} alt="" className="rounded-full w-[45px]" />
                 <h3 className="text-xl font-normal text-[#012269CC] hover:outline p-2 rounded-2xl outline-1">
                     {/* Halo {`${ubahProfileViewModel.formik.values.usernameBaru}`} */}
-                    Halo {loginViewModel.formik.values.name}
+                    {/* Halo {loginViewModel.formik.values.name} */}
+                    Halo Admin 1
                 </h3>
             </button>
         </Link>
