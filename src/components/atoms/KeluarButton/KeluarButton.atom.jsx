2ea597c6e44e41556a4f0ab4f4c56e9a744226bf
eldavina <125462461+eldavina@users.noreply.github.com>
2023-06-23 17:08:14 +0700

    Update KeluarButton.atom.jsx

--- src/components/atoms/KeluarButton/KeluarButton.atom.jsx
+++ src/components/atoms/KeluarButton/KeluarButton.atom.jsx
@@ -1,16 +1,21 @@
 import React from "react"
 import logout from "../../../assets/logout.png"
 
-function KeluarButton() {
+function KeluarButton({ onClick }) {
     return (
-        <div className="pr-16">
-            <button
-                className="flex items-center gap-2 hover:outline p-2 rounded-2xl outline-1 outline-[#DA0000CC] "
-                type="button"
-                id="buttonKeluar">
-                <img src={logout} alt="" />
-                <h3 className="text-xl font-bold text-[#DA0000CC] ">Keluar</h3>
-            </button>
+        <div>
+            <div className="pr-16">
+                <button
+                    className="flex items-center gap-2 hover:outline p-2 rounded-2xl outline-1 outline-[#DA0000CC] "
+                    type="button"
+                    id="buttonKeluar"
+                    onClick={onClick}>
+                    <img src={logout} alt="" />
+                    <h3 className="text-xl font-bold text-[#DA0000CC] ">
+                        Keluar
+                    </h3>
+                </button>
+            </div>
         </div>
     )
 }
