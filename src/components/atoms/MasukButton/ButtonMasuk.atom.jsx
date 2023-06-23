6f2c17e2de9a58ba5d3b0e417791bfb890e172e5
eldavina <125462461+eldavina@users.noreply.github.com>
2023-06-23 15:38:23 +0700

    Update ButtonMasuk.atom.jsx

--- src/components/atoms/MasukButton/ButtonMasuk.atom.jsx
+++ src/components/atoms/MasukButton/ButtonMasuk.atom.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function ButtonMasuk({ textButton }) {
+function ButtonMasuk({ textButton,type }) {
 	return (
 		<button
 			className="bg-[#195FAA] py-[8px] px-[32px] text-white rounded-2xl text-base font-black"
-			type="submit"
+			type={type}
 			id="buttonSimpanMasuk">
 			{`${textButton === "simpan" ? "Simpan" : "Masuk"}`}
 		</button>
