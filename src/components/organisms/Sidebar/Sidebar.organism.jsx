4dca305ecadc390f2ddea53b4cbbb67acd502058
dimasandriano <duangdeong@gmail.com>
2023-06-25 16:47:17 +0700

    fix: icon online, sidebar

--- src/components/organisms/Sidebar/Sidebar.organism.jsx
+++ src/components/organisms/Sidebar/Sidebar.organism.jsx
@@ -5,7 +5,7 @@ function Sidebar({ expand, handleExpand }) {
     return (
         <div
             className={`fixed top-[89px] ${
-                expand ? "w-[231px] z-10" : "w-[83px]"
+                expand ? "w-[231px] z-9" : "w-[83px]"
             } h-[80vh] bg-white rounded-tr-[20px] shadow-md drop-shadow-xl rounded-br-[20px] pt-[20px] px-[5px] flex flex-col gap-[10px] border-[1px] transition-all duration-100`}>
             <div
                 className="py-1 px-6 flex justify-center items-center w-full h-[52px] rounded-2xl cursor-pointer hover:border-[1px] hover:border-[#195FAA] group transition"
